0e87c29d16ceece43a72e323d7da90ee39fb217b
Thomas Anderson <Thomas Anderson@DESKTOP-VSDQQHA>
2016-04-19 10:46:15 -0400

    Add project files.

---
+++ MasterDetail.Business/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterDetail.Business
+{
+    public class Class1
+    {
+    }
+}
